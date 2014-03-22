b7fba95bf8eeda1a52a396c825fc047a76139627
David Read <d.j.read-09@student.lboro.ac.uk>
2014-03-22 18:57:49 +0000

    nicer control

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,22 +6,18 @@ public class CharacterController : MonoBehaviour {
 	public float moveSpeed = 10f;			// movespeed of the player
 
 	public float sensitivityX = 15F;		// mouse sensitivity
-	public float sensitivityY = 15F;
-	
-	public float minimumY = -60F;			// locked Y axis view
-	public float maximumY = 60F;
 
-	float rotationY = 0F;
+	void Start ()
+	{
+		Screen.lockCursor = true;
+	}
 
 	void Update ()
 	{
 		// Get mouse input and move the camera rotation
 		float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
 		
-		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-		rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-		
-		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		transform.localEulerAngles = new Vector3(0, rotationX, 0);
 	}
 
 	// Update is called once per frame
@@ -30,8 +26,7 @@ public class CharacterController : MonoBehaviour {
 		// Take keyboard input WSAD
 		float h = Input.GetAxisRaw("Horizontal");
 		float v = Input.GetAxisRaw("Vertical");
-
-
+		
 		// Create a vector and populate it with the movement input
 		Vector3 force = (transform.forward * v) + (transform.right * h);
 		force.Normalize();
---
+++ Assets/Scripts/CharacterLook.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterLook : MonoBehaviour {
+
+	public float sensitivityY = 15F;		// mouse sensitivity
+	
+	public float minimumY = -60F;			// locked Y axis view
+	public float maximumY = 60F;
+	
+	float rotationY = 0F;
+	
+	void Update ()
+	{
+		// Get mouse input and move the camera rotation
+
+		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+		rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+		
+		transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+	}
+}
