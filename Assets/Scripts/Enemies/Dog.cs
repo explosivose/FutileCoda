2d67d7ec5553e3bf1d2b3ff36973ba7bd8af4b2b
mattblickem <explosivose@gmail.com>
2014-03-22 21:38:14 +0000

    dog animations

--- Assets/Scripts/Enemies/Dog.cs
+++ Assets/Scripts/Enemies/Dog.cs
@@ -8,17 +8,21 @@ public class Dog : MonoBehaviour
 	public float moveSpeed;
 	public int hitPoints = 10;
 	
+	private bool isDead = false;
 	private Transform player;
+	private Animator anim;
 
 	// Use this for initialization
 	void Awake () 
 	{
 		player = GameObject.FindGameObjectWithTag("Player").transform;
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
+		if (isDead) return;
 		Vector3 target = player.position;
 		target.y = transform.position.y;
 		Vector3 direction = target - transform.position;
@@ -28,19 +32,49 @@ public class Dog : MonoBehaviour
 	
 	void OnCollisionEnter(Collision col)
 	{
+		if (isDead) return;
 		if (col.gameObject.tag == "Bullet")
 		{
 			if ( col.relativeVelocity.magnitude > 10f )
 			{
-				hitPoints--;
+				StartCoroutine(Hurt());
 				if (hitPoints < 0) StartCoroutine(Die());
 			}
 		}
+		if (col.gameObject.tag == "Player")
+		{
+			anim.Play("dog_attack");
+		}
 	}
+	
+	void OnCollisionExit(Collision col)
+	{
+		if (isDead) return;
+		if (col.gameObject.tag == "Player")
+		{
+			anim.Play ("dog_walkcycle");
+		}
+			
+	}
+	
+	IEnumerator Hurt()
+	{
+		if (!isDead)
+		{
+			hitPoints--;
+			anim.Play("dog_hit");
+			yield return new WaitForSeconds(0.4f);
+			anim.Play ("dog_walkcycle");
+		}
+		yield return new WaitForFixedUpdate();
+	}
+		
 	
 	IEnumerator Die()
 	{
-		yield return new WaitForFixedUpdate();
+		isDead = true;
+		anim.Play("dog_death");
+		yield return new WaitForSeconds(1f);
 		Destroy(this.gameObject);
 	}
 }
---
+++ Assets/Scripts/Weapons/Laser.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour 
+{
+	public bool debug = false;
+	public AudioClip firingSound;
+	public float chargeTime = 10f; 		// time to charge from 0 to full
+	public Transform particleEffect;
+	
+	private float charge = 10f;
+	private Transform player;
+	private bool firing = false;
+	private float lastFireTime = 0f;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		particleEffect.parent = Camera.main.transform;
+		particleEffect.localPosition = Vector3.zero;
+		particleEffect.localRotation = Quaternion.identity;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	public void Fire()
+	{
+		lastFireTime = Time.time;
+		if (!firing) StartCoroutine(FireLaser());
+	}
+	
+	IEnumerator FireLaser()
+	{
+		firing = true;
+		while (lastFireTime + 1f > Time.time)
+		{
+			
+			yield return new WaitForFixedUpdate();
+		}
+		firing = false;
+	}
+	
+	
+}
