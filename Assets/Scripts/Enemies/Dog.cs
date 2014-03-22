1dc93e38df6644e2fcb094c43bed7ac30070c0c3
mattblickem <explosivose@gmail.com>
2014-03-22 19:55:56 +0000

    dog now dies, bullets move faster

--- Assets/Scripts/Enemies/Dog.cs
+++ Assets/Scripts/Enemies/Dog.cs
@@ -6,6 +6,7 @@ using System.Collections;
 public class Dog : MonoBehaviour 
 {
 	public float moveSpeed;
+	public int hitPoints = 10;
 	
 	private Transform player;
 
@@ -23,5 +24,23 @@ public class Dog : MonoBehaviour
 		Vector3 direction = target - transform.position;
 		transform.rotation = Quaternion.LookRotation(direction);
 		rigidbody.AddForce(transform.forward * moveSpeed * Time.deltaTime, ForceMode.VelocityChange);
+	}
+	
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Bullet")
+		{
+			if ( col.relativeVelocity.magnitude > 10f )
+			{
+				hitPoints--;
+				if (hitPoints < 0) StartCoroutine(Die());
+			}
+		}
+	}
+	
+	IEnumerator Die()
+	{
+		yield return new WaitForFixedUpdate();
+		Destroy(this.gameObject);
 	}
 }
