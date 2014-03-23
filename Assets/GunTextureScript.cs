3c01ff50d25fcd95c4c86c36c1d1b58177af2c09
David Read <d.j.read-09@student.lboro.ac.uk>
2014-03-23 19:23:44 +0000

    gun texture added

---
+++ Assets/GunTextureScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunTextureScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//guiTexture.enabled = false;
+		int textureHeight = guiTexture.texture.height;
+		int textureWidth = guiTexture.texture.width;
+		int screenHeight = Screen.height;
+		int screenWidth = Screen.width;
+
+		int textureHeightScaled = Mathf.RoundToInt (screenHeight/2);
+		int	textureWidthScaled = Mathf.RoundToInt (screenWidth/2);
+		Debug.Log (textureWidthScaled);
+		Debug.Log (textureHeightScaled);
+		guiTexture.pixelInset = new Rect(-textureWidthScaled/2, -screenHeight/2,textureWidthScaled, textureHeightScaled);
+		 ;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	if(!GameObject.Find ("GameManager").GetComponent<GameManager>().GameIsPaused)
+			guiTexture.enabled = true;
+	}
+}
